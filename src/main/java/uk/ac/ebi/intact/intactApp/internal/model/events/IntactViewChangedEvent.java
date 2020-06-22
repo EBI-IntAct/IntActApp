53ed39ba2343aee16f69e6fef3f73a033cc6c8f5
EliotRagueneau <orkeliot@gmail.com>
2020-06-22 20:13:08 +0100

    IntactViewChanged events handling

--- src/main/java/uk/ac/ebi/intact/intactApp/internal/model/events/IntactViewChangedEvent.java
+++ src/main/java/uk/ac/ebi/intact/intactApp/internal/model/events/IntactViewChangedEvent.java
@@ -4,11 +4,13 @@ import org.cytoscape.event.AbstractCyEvent;
 import uk.ac.ebi.intact.intactApp.internal.model.managers.IntactManager;
 import uk.ac.ebi.intact.intactApp.internal.model.IntactNetworkView;
 
-public class IntactViewTypeChangedEvent extends AbstractCyEvent<IntactManager> {
+public class IntactViewChangedEvent extends AbstractCyEvent<IntactManager> {
     public final IntactNetworkView.Type newType;
+    public final IntactNetworkView view;
 
-    public IntactViewTypeChangedEvent(IntactManager source, IntactNetworkView.Type newType) {
+    public IntactViewChangedEvent(IntactManager source, IntactNetworkView view) {
         super(source, IntactViewTypeChangedListener.class);
-        this.newType = newType;
+        this.newType = view.getType();
+        this.view = view;
     }
 }
