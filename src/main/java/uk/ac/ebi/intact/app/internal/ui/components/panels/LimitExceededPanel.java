f1b7ca232d6ed27a85a6f0de5d07566190f546dc
EliotRagueneau <orkeliot@gmail.com>
2020-07-09 11:22:57 +0100

    Modifiable LimitExceededPanel

--- src/main/java/uk/ac/ebi/intact/app/internal/ui/components/panels/LimitExceededPanel.java
+++ src/main/java/uk/ac/ebi/intact/app/internal/ui/components/panels/LimitExceededPanel.java
@@ -5,20 +5,36 @@ import java.awt.*;
 
 public class LimitExceededPanel extends JPanel {
 
+    private String limitOfWhat;
+    private String solution;
+    private String limitedState;
+    private int limit;
+    private Font font;
+    private int horizontalAlignment;
+
     public LimitExceededPanel(String limitOfWhat, String limitedState, int limit, String solution) {
         this(limitOfWhat, limitedState, limit, solution, JLabel.CENTER);
     }
 
     public LimitExceededPanel(String limitOfWhat, String limitedState, int limit, String solution, int horizontalAlignment) {
+        this.limit = limit;
+        this.limitOfWhat = limitOfWhat;
+        this.limitedState = limitedState;
+        this.solution = solution;
+        this.horizontalAlignment = horizontalAlignment;
         setLayout(new GridLayout(2, 1));
-        Font font = getFont().deriveFont(15f);
-        JLabel label = new JLabel(String.format("More than %d %s %s", limit, limitOfWhat, limitedState));
+        font = getFont().deriveFont(15f);
+    }
+
+    private void setupLabels() {
+        removeAll();
+        JLabel label = new JLabel(String.format("More than %d %s %s", this.limit, this.limitOfWhat, this.limitedState));
         label.setForeground(Color.WHITE);
         label.setFont(font);
         label.setHorizontalAlignment(horizontalAlignment);
         label.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
         add(label);
-        JLabel label1 = new JLabel(String.format("For details on other %s, please %s", limitOfWhat, solution));
+        JLabel label1 = new JLabel(String.format("For details on other %s, please %s", this.limitOfWhat, this.solution));
         label1.setFont(font);
         label1.setForeground(Color.WHITE);
         label1.setHorizontalAlignment(horizontalAlignment);
@@ -34,11 +50,52 @@ public class LimitExceededPanel extends JPanel {
         Insets insets = new Insets(0, 0, 0, 0);
         insets = getInsets(insets);
         int height = getHeight() - insets.top - insets.bottom;
-//            g2.setPaint(new LinearGradientPaint(0, 0, 0, height,
-//                    new float[]{0.0f, 1f},
-//                    new Color[]{new Color(66, 42, 146), new Color(134, 56, 148),}));
         g2.setPaint(new Color(161, 135, 184));
         g2.fillRoundRect(insets.left, insets.top, getWidth() - insets.left - insets.right, height, 10, 10);
     }
 
+    public String getLimitOfWhat() {
+        return limitOfWhat;
+    }
+
+    public void setLimitOfWhat(String limitOfWhat) {
+        this.limitOfWhat = limitOfWhat;
+        setupLabels();
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+        setupLabels();
+    }
+
+    public String getLimitedState() {
+        return limitedState;
+    }
+
+    public void setLimitedState(String limitedState) {
+        this.limitedState = limitedState;
+        setupLabels();
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+        setupLabels();
+    }
+
+    public int getHorizontalAlignment() {
+        return horizontalAlignment;
+    }
+
+    public void setHorizontalAlignment(int horizontalAlignment) {
+        this.horizontalAlignment = horizontalAlignment;
+        setupLabels();
+    }
 }
