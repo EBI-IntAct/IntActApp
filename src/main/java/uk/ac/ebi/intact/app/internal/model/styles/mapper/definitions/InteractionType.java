bc7f2fe25c44dedd121836261274fcf400da4d80
EliotRagueneau <orkeliot@gmail.com>
2020-07-09 10:52:10 +0100

    Viridis color palette for expanded edges

--- src/main/java/uk/ac/ebi/intact/app/internal/model/styles/mapper/definitions/InteractionType.java
+++ src/main/java/uk/ac/ebi/intact/app/internal/model/styles/mapper/definitions/InteractionType.java
@@ -4,13 +4,13 @@ import java.awt.*;
 
 public enum InteractionType {
     COLOCOALIZATION("colocalization", "MI_0403", new Color(165, 165, 165), false),
-    ASSOCIATION("association", "MI_0914", new Color(97, 131, 196), false),
-    PHYSICAL_ASSOCIATION("physical association", "MI_0915", new Color(178, 101, 188), false),
-    DIRECT_INTERACTION("direct interaction", "MI_0407", new Color(184, 54, 75), true),
-    PHOSPHORYLATION_R("phosphorylation reaction", "MI_0217", new Color(231, 111, 61), true),
-    DEPHOSPHORYLATION_R("dephosphorylation reaction", "MI_0203", new Color(231, 111, 61), true),
-    PHOSPHORYLATION("phosphorylation", "", new Color(231, 111, 61), false),
-    DEPHOSPHORYLATION("dephosphorylation", "", new Color(231, 111, 61), false);
+    ASSOCIATION("association", "MI_0914", new Color(73, 60, 136), false),
+    PHYSICAL_ASSOCIATION("physical association", "MI_0915", new Color(89, 121, 195), false),
+    DIRECT_INTERACTION("direct interaction", "MI_0407", new Color(71, 172, 159), true),
+    PHOSPHORYLATION_R("phosphorylation reaction", "MI_0217", new Color(139, 212, 81), true),
+    DEPHOSPHORYLATION_R("dephosphorylation reaction", "MI_0203",new Color(139, 212, 81), true),
+    PHOSPHORYLATION("phosphorylation", "", new Color(139, 212, 81), false),
+    DEPHOSPHORYLATION("dephosphorylation", "", new Color(139, 212, 81), false);
 
     public final String name;
     public final String MI_ID;
