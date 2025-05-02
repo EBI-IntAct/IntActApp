4856d2903ec54c163e4788de90911616125b6f92
Susie Huget <susiehuget@ebi.ac.uk>
2025-05-02 16:52:12 +0100

    Adding of ortholog group filter;

--- src/main/java/uk/ac/ebi/intact/app/internal/model/managers/Manager.java
+++ src/main/java/uk/ac/ebi/intact/app/internal/model/managers/Manager.java
@@ -9,7 +9,8 @@ import java.util.Properties;
 
 public class Manager {
 //    private static final String INTACT_WEBSERVICES = "https://www.ebi.ac.uk/intact/ws/";
-    private static final String INTACT_WEBSERVICES = "https://wwwdev.ebi.ac.uk/intact/ws/";
+//    private static final String INTACT_WEBSERVICES = "https://wwwdev.ebi.ac.uk/intact/ws/";
+    private static final String INTACT_WEBSERVICES = "http://localhost:8083/intact/ws/";
 
     public static final String INTACT_GRAPH_WS = INTACT_WEBSERVICES + "graph/";
     public static final String INTACT_INTERACTOR_WS = INTACT_WEBSERVICES + "interactor/";
