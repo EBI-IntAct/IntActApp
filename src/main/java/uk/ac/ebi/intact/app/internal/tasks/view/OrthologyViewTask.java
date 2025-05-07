eca37bd54158340c3b224a6080aebfa1ca41deca
Susie Huget <susiehuget@ebi.ac.uk>
2025-05-07 11:29:12 +0100

    Adding of ortholog group as a view instead of filter (seems stable, further tests to do);

---
+++ src/main/java/uk/ac/ebi/intact/app/internal/tasks/view/OrthologyViewTask.java
@@ -0,0 +1,23 @@
+package uk.ac.ebi.intact.app.internal.tasks.view;
+
+import org.cytoscape.task.hide.HideTaskFactory;
+import org.cytoscape.task.hide.UnHideTaskFactory;
+import org.cytoscape.work.TaskMonitor;
+
+import uk.ac.ebi.intact.app.internal.model.core.view.NetworkView;
+import uk.ac.ebi.intact.app.internal.model.managers.Manager;
+
+public class OrthologyViewTask extends AbstractHiderTask {
+
+    public OrthologyViewTask(Manager manager, HideTaskFactory hideTaskFactory, UnHideTaskFactory unHideTaskFactory, boolean currentView) {
+        super(manager, hideTaskFactory, unHideTaskFactory, currentView);
+    }
+
+    @Override
+    public void run(TaskMonitor taskMonitor) throws Exception {
+//        collapseEdgesIfNeeded();
+        if (chosenView != null && chosenView.getType() != NetworkView.Type.ORTHOLOGY) {
+            manager.data.viewChanged(NetworkView.Type.ORTHOLOGY, chosenView);
+        }
+    }
+}
