7948d69119c1d23b7f0eff9f64526a05d6bb5fc1
jmedinaebi <jmedina@ebi.ac.uk>
2025-07-21 15:16:12 +0100

    Revert "Remove unnecessary call to filter"
    This reverts commit fed3f27ee58111301f42b8e0a2b037db7a39f2ba.

--- src/main/java/uk/ac/ebi/intact/app/internal/tasks/view/parameters/OrthologyViewParameterTask.java
+++ src/main/java/uk/ac/ebi/intact/app/internal/tasks/view/parameters/OrthologyViewParameterTask.java
@@ -56,6 +56,7 @@ public class OrthologyViewParameterTask extends AbstractViewTask {
                 resetLayout(monitor);
             } else {
                 chosenNetwork.collapseGroups(groups);
+                chosenView.filter(); // Need to filter after collapsing as collapsing set the child node visibility to true
                 applyParameterLayout(monitor);
             }
         } else {
